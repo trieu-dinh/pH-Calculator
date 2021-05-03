543b0602ee3e2c715f134dbc6133a4fb38a985c8
Trieu-Dinh <trieu.dinh2014@yahoo.com>
2021-05-03 12:24:03 -0500

    Worked on the Quit toggle button, needs more work

--- finalLab.m
+++ finalLab.m
@@ -12,19 +12,22 @@ pH.maText = uicontrol('Style','text','String',' Molarity Acid:','position',[60 2
 pH.mbText = uicontrol('Style','text','String',' Molarity Base:','position',[60 235 50 40],'units','normalized');
 pH.vaText = uicontrol('Style','text','String',' Volume Acid:','position',[60 185 50 40],'units','normalized');
 pH.vbText = uicontrol('Style','text','String',' Volume Base:','position',[60 135 50 40],'units','normalized');
-pH.outputText = uicontrol('Style','text','String','Output: ','position',[200 200 50 40],'units','normalized');
-pH.calc = uicontrol('style','pushbutton','position', [60 85 75 35],'string',' Calculate ','callback',{@selection},'units','normalized'); % the calculate button
+pH.outputText = uicontrol('Style','text','String','Output: ','position',[200 250 80 80],'units','normalized');
+pH.calc = uicontrol('style','pushbutton','position', [60 85 75 35],'string',' Calculate ','callback',{@selection},'units','normalized'); % the calculate button, callback to the selection function
+pH.quit = uicontrol('style','togglebutton','position',[200 85 75 35],'string','Quit','units','normalized','callback',{@quitGUI}); % the button to quit the GUI
 end
-function [] = selection(~,~)
+function [] = selection(~,~) %The selection function takes the inputted values from the edit buttons and uses them to calculate and ouput the pH
 global pH;
 Ma = str2double(pH.molarityAcidEdit.String);
 Mb = str2double(pH.molarityBaseEdit.String);
 Va = str2double(pH.volumeAcidEdit.String);
 Vb = str2double(pH.volumeBaseEdit.String);
 pH.calcResult = CalculatePH(Ma,Mb,Va,Vb);
-pH.outputText.String = ['output: ' num2str(pH.calcResult)];
+pH.outputText.String = ['Output: ' num2str(pH.calcResult)];
 end
-
+function quitGUI(~,~)  %The quitGUI function exits out of the GUI when toggled.
+    close(fig)
+end
 function [pH] = CalculatePH(Ma,Mb,Va,Vb) %CalculatePH calculates the pH for a strong acid/base dissociation
 z = (Ma*Va - Mb*Vb)/(Va+Vb);
 Kw = 1e-14; %equilibrium constant (water)
